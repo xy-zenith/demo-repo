83df3a4e89645917e8274680cf55c0e272425be2
Kapi <kapi@sfsu.edu>
2024-09-26 11:01:43 -0700

    add express starter

---
+++ express/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
