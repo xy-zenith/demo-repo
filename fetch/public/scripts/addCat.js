671dc428b592fcc043929225f4464f6a5d7496bc
Erika Lee <erika.lee@codepath.org>
2024-10-12 15:29:14 -0700

    add fetch demo

---
+++ fetch/public/scripts/addCat.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector('form');
+
+});
---
+++ fetch/public/scripts/deleteCat.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const container = document.querySelector('.container');
+    const buttons = container.querySelectorAll('button');
+
+});
