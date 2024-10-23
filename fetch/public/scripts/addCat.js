65d2f2c109ac3e6f6e2047dc32d45675c36fb3e3
unknown <a.keithtang@gmail.com>
2024-10-22 22:33:13 -0700

    fetch

--- fetch/public/scripts/addCat.js
+++ fetch/public/scripts/addCat.js
@@ -1,4 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.querySelector('form');
+    form.addEventListener("submit", (event) => 
+    {
+        event.preventDefault();
+        const name = document.getElementById("name").value;
+        const color = document.getElementById("color").value;
+        const human = document.getElementById("human").value;
+        const image = document.getElementById("image-url").value;
 
+        fetch("/api/cats", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ name, color, human, image }), //shorthand of name: name, color: color...
+        }).then(response => {
+            return response.json();
+        }).then(data => {
+            console.log(data);
+            // window.location.href = `/cats/${data.id}`;
+        }).catch(error => {
+            console.error("error: ", error);
+        });
+    });
 });
--- fetch/public/scripts/deleteCat.js
+++ fetch/public/scripts/deleteCat.js
@@ -1,5 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
     const container = document.querySelector('.container');
     const buttons = container.querySelectorAll('button');
-
+    for(let button of buttons)
+    {
+        button.addEventListener("click", (event) => 
+        {
+            let catId = button.id.split("-")[1]; //split = {"delete", "0"}
+            console.log(catId);
+            fetch(
+                `/api/cats/${catId}`, 
+                {
+                    method: "DELETE"
+                }
+            ).then(response => {
+                return response.json();
+            }).then(data => {
+                console.log(data);
+                window.location.reload();
+            }).catch(error => {
+                console.error("error", error);
+            })
+        });
+    }
 });
