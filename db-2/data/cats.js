bff3a52539f5912e2e04e34e6f954ebec13b4f83
Erika Lee <erika.lee@codepath.org>
2024-10-27 21:51:14 -0700

    add async and db-2

---
+++ db-2/data/cats.js
@@ -0,0 +1,24 @@
+import { pool } from '../config/database.js';
+
+const getCats = async () => {
+
+};
+
+const getCat = async (id) => {
+
+};
+
+
+const createCat = async (data) => {
+
+};
+
+const updateCat = async (id, data) => {
+
+};
+
+const deleteCat = async (id) => {
+
+};
+
+export {getCats, getCat, createCat, updateCat, deleteCat};
