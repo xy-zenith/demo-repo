812840eae5033b152f570eea6ccb34061e29bc17
unknown <a.keithtang@gmail.com>
2024-11-12 11:53:25 -0800

    update website header

--- async/async.js
+++ async/async.js
@@ -1,16 +1,25 @@
-const doFetch = () => {
-//const doFetch = async () => {
+// const doFetch = () => {
+const doFetch = async () => {
     console.log("fetching from API...")
     const startTime = Date.now();
 
-    const response = fetch('https://fakestoreapi.com/products?limit=1')
+    // const response = fetch('https://fakestoreapi.com/products?limit=1')
+    // .then(response => {
+    //     let elapsedTime = Date.now() - startTime;
+    //     console.log(`time elapsed since start for query: ${elapsedTime} ms`);
+    //     console.log(`response:`);
+    //     console.log(response);    
+    // })
+
+    const response = await fetch('https://fakestoreapi.com/products?limit=1')
     .then(response => {
         let elapsedTime = Date.now() - startTime;
         console.log(`time elapsed since start for query: ${elapsedTime} ms`);
         console.log(`response:`);
         console.log(response);    
     })
-    //const response = await fetch('https://fakestoreapi.com/products?limit=1');
+
+    // const response = await fetch('https://fakestoreapi.com/products?limit=1');
 
 
     // const data = await response.json()
